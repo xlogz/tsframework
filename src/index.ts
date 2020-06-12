0481a78013d074f224780cc22df6a0bc5a9f41fa
xlogz <loganxlow@gmail.com>
2020-06-11 19:21:48 -0700

    Added event handlers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
+
+const user = User.buildUser({name: 'Logan', age: 29});
+
+const userForm = new UserForm(
+    document.getElementById('root')
+, user);
+
+userForm.render();
