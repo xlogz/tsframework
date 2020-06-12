093761497495a09605bb43dc65f7cf02944ba0f0
xlogz <loganxlow@gmail.com>
2020-06-11 18:46:15 -0700

    Added user collection creation and user creation

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Collection } from './models/Collection';
+import { User,UserProps } from './models/User';
+
+
+const collection = User.buildUserCollection();
+
+collection.on('change',()=>{
+    console.log(collection)
+});
+
+collection.fetch();
---
+++ src/models/ApiSync.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosPromise } from 'axios';
+
+interface HasId{
+    id?: number;
+}
+
+
+export class ApiSync<T extends HasId>{
+
+    constructor( public rootUrl: string) {
+
+    }
+
+    fetch(id: number): AxiosPromise{
+       return  axios.get(`${this.rootUrl}/${id}`);
+    }
+
+    save(data: T): AxiosPromise {
+        const { id } = data;
+        //put
+        if(id){
+            return axios.put(`${this.rootUrl}/${id}`, data);
+        }else{
+            return axios.post(`${this.rootUrl}`, data);
+        }
+            
+    }
+}
